8d9a4ea4f4e93d6864bfddef14644a24bfbadea5
Joel Carpenter <aldosac@gmail.com>
2020-12-29 14:38:47 -0800

    Adds tests for session state

--- test/tests/App.test.jsx
+++ test/tests/App.test.jsx
@@ -1,9 +1,29 @@
 import App from "../../src/components/App.jsx";
+import { useState as useStateMock } from "react";
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn()
+}));
 
 describe("App", () => {
+  const setStateMock = jest.fn();
 
-  it("should render a component", () => {
+  afterAll(() => {
+    jest.restoreAllMocks();
+  })
+
+  it("should ask for a session id if none exists", () => {
+    useStateMock.mockImplementation( () => [null, setStateMock])
     const wrapper = shallow(<App />);
+    
+    expect(wrapper.is("#app")).toBe(true);
+  });
+
+  it("should render main app if session id exists", () => {
+    useStateMock.mockImplementation( () => ["XKCL", setStateMock])
+    const wrapper = shallow(<App />);
+
     expect(wrapper.is("#app")).toBe(true);
   });
 });
