363faa3840dcfe71631df14442db933bb959bb73
Joel Carpenter <aldosac@gmail.com>
2021-01-05 17:24:50 -0800

    Adds "Reset Groups" button

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -10,12 +10,16 @@ const Groups = (props) => {
     addGroup,
     deleteGroup,
     clearGroups,
+    resetGroups
   } = props;
 
   return (
     <div className="groups-container" >
       <div className="groups-controls-container">
-        <button className="btn-clear-groups" onClick={clearGroups} >Clear Groups</button>
+        <div className="clear-reset-container" >
+          <button className="btn-clear-groups" onClick={clearGroups} >Clear Groups</button>
+          <button className="btn-reset-groups" onClick={resetGroups} >Reset Groups</button>
+        </div>
         <div className="add-generate-container">
           <AddGroups addGroup={addGroup} />
           <GenerateGroups
